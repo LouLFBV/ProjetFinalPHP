086395fe0b7c57c90dacd476978c5dee8e013eee
LouLFBV <lou.lefebvre@ynov.com>
2026-02-03 10:22:23 +0100

    First commit

---
+++ App/index.php
@@ -0,0 +1,14 @@
+<?php
+$status = "Le serveur intégré PHP fonctionne !";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Test E-Commerce</title>
+</head>
+<body>
+    <h1><?php echo $status; ?></h1>
+    <p>Prêt pour le projet final PHP.</p>
+</body>
+</html>
